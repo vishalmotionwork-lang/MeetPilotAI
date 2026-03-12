8af5848f69edeecd5db455dcb39d810d46bb4afe
SejalDaroliya <sejal.daroliya04@gmail.com>
2026-03-12 23:08:47 +0530

    changed the app.py file

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from config.db import get_db_connection
 
@@ -18,4 +19,5 @@ def test_db():
     return {"database_time": result}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
