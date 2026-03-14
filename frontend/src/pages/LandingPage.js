813ac256ed713306614e53010d7e90d00701ad99
SejalDaroliya <sejal.daroliya04@gmail.com>
2026-03-14 12:21:53 +0530

    updated the landing page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -4,7 +4,7 @@ function LandingPage() {
   return (
     <div>
       <h1>AI Meeting Minutes Generator</h1>
-      <p>Convert meeting transcripts into smart summaries. This is our final year project. We are  building something new.</p>
+      <p>Convert meeting transcripts into smart summaries. This is our final year project. We are  building something new. Something great is happening</p>
     </div>
   );
 }
