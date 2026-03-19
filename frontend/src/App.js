b736b80086cabb99e3aad6759cd5032b9515a86e
SejalDaroliya <sejal.daroliya04@gmail.com>
2026-03-19 09:44:14 +0530

    login signup functionality implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
@@ -7,17 +9,21 @@ import SummaryPage from "./pages/SummaryPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignUpPage";
 
-
 function App() {
   return (
     <BrowserRouter>
+
+      {/* ✅ Toast container must be inside return */}
+      <ToastContainer position="top-right" autoClose={2000} />
+
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/summary" element={<SummaryPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />}/>
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
